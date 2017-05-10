24aada2944cf3ec2aa264d97f8929f50a138be8d
Steve Nelson <stevenn@middlebury.edu>
2017-05-10 16:10:35 -0400

    first deployment

---
+++ vendor/acquia/blt/src/Robo/Commands/Tests/AllCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Acquia\Blt\Robo\Commands\Tests;
+
+use Acquia\Blt\Robo\BltTasks;
+
+/**
+ * Defines commands in the "tests" namespace.
+ */
+class AllCommand extends BltTasks {
+
+  /**
+   * Runs all tests, including Behat, PHPUnit, and Security Update check.
+   *
+   * @command tests:all
+   */
+  public function tests() {
+    $status_code = $this->invokeCommands([
+      'tests:behat',
+      'tests:phpunit',
+      'tests:security-updates',
+    ]);
+
+    return $status_code;
+  }
+
+}
